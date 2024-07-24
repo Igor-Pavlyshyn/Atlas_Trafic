1b46237a7e9c599359369813662b8b4ef5defcc7
Igor Pavlyshyn <igor.pavlyshyn.work@gmail.com>
2024-07-24 17:21:14 +0300

    fetch all intersections at once and then creates cars for each intersection and classification

--- atlas_trafic_app/management/commands/add_car_data.py
+++ atlas_trafic_app/management/commands/add_car_data.py
@@ -9,35 +9,18 @@ class Command(BaseCommand):
     help = "Add car data to the database"
 
     def handle(self, *args, **kwargs):
-        for i in range(1, 3):
-            Car.objects.create(
-                classification="Passenger Vehicle",
-                count=random.randint(1, 12),
-                detected_at=datetime.now() + timedelta(hours=2),
-                intersection=Intersection.objects.get(id=i),
-            )
-            Car.objects.create(
-                classification="Heavy Truck",
-                count=random.randint(1, 12),
-                detected_at=datetime.now() + timedelta(hours=2),
-                intersection=Intersection.objects.get(id=i),
-            )
-            Car.objects.create(
-                classification="Public Transportation",
-                count=random.randint(1, 12),
-                detected_at=datetime.now() + timedelta(hours=2),
-                intersection=Intersection.objects.get(id=i),
-            )
-            Car.objects.create(
-                classification="Pedestrian",
-                count=random.randint(1, 12),
-                detected_at=datetime.now() + timedelta(hours=2),
-                intersection=Intersection.objects.get(id=i),
-            )
-            Car.objects.create(
-                classification="Micromobility User",
-                count=random.randint(1, 12),
-                detected_at=datetime.now() + timedelta(hours=2),
-                intersection=Intersection.objects.get(id=i),
-            )
-            print(f"Added car data for intersection {i}")
+        intersections = Intersection.objects.all()
+        for intersection in intersections:
+            classifications = ["Passenger Vehicle",
+                               "Heavy Truck",
+                               "Public Transportation",
+                               "Pedestrian",
+                               "Micromobility User"]
+            for classification in classifications:
+                count = random.randint(1, 12)
+                Car.objects.create(
+                    classification=classification,
+                    count=count,
+                    intersection=intersection,
+                )
+            print(f"Added car data for intersection {intersection.id}")
